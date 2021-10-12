cf20217782a71c0a45c85b43a27a36bf479df429
RyanYJOh <hiimryan@kakao.com>
2021-10-12 21:37:44 +0900

    filter revised

--- museum/main/views.py
+++ museum/main/views.py
@@ -121,7 +121,7 @@ def main_page_filtered(request, question_no):
     
     ## Pagination을 쓸 것이냐.
     this_question = QuestionsFromUs.objects.get(question_no=question_no).pk
-    all_ans_us_filtered = AnswersForFromUs.objects.filter(question_id=this_question)
+    all_ans_us_filtered = AnswersForFromUs.objects.filter(is_shared=True, question_id=this_question).order_by('-created_at_time')
     paginator_all_ans_us = Paginator(all_ans_us_filtered, 15)
     page_all_ans_us = request.GET.get('page')
     all_ans_us_paginated = paginator_all_ans_us.get_page(page_all_ans_us)
