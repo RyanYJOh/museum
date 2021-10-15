783e8c3822a59afd596223422d5247d9a698ad85
RyanYJOh <hiimryan@kakao.com>
2021-10-15 22:44:28 +0900

    admin sorting, pagination repositioned, width, x button

--- museum/register/admin.py
+++ museum/register/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 # Register your models here.
+class MyUserAdmin(UserAdmin):
+    ordering = ('-date_joined',)
+    list_display = ('username', 'date_joined')
+
+admin.site.unregister(User)
+admin.site.register(User, MyUserAdmin)
