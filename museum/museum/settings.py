a3944f21468a4e7e600189417b145062f67c0d3b
RyanYJOh <hiimryan@kakao.com>
2022-03-12 18:20:25 +0900

    debug

--- museum/museum/settings.py
+++ museum/museum/settings.py
@@ -14,8 +14,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True # 로컬
-# DEBUG = False # 운영
+# DEBUG = True # 로컬
+DEBUG = False # 운영
 
 
 ALLOWED_HOSTS = [
